d6ecdf49f1070066ebcceccea2ad8417d1956ea6
jiangbo9510 <jiangbo1234321@gmail.com>
2024-01-09 11:21:58 +0800

    Update base.go

--- base.go
+++ base.go
@@ -4,6 +4,14 @@ var (
 	Common = ""
 )
 
-func GetString() string {
-	return "v0.0.2"
+func GetString() (string, string) {
+	return "v", "0.0.2"
+}
+
+func GetCommon() string {
+	return Common
+}
+
+func SetCommon(v string) {
+	Common = v
 }
